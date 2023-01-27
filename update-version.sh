42e17c36f5c40916ceab1f950623394d8df93eb3
annied471 <annied7@uci.edu>
2023-01-27 01:33:03 -0800

    Test commit

---
+++ update-version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo $(date '+%Y-%m-%d %H:%M:%S') > version
+
+cd /cogs106/hw1
+git add --all
+git commit -m "Test commit"
+git push https://github.com/annied471/cogs106.git master
+
+
