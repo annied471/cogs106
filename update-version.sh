82ebfa6cbe67e897fdff3e8e73135af86308151b
annied471 <annied7@uci.edu>
2023-01-27 01:55:04 -0800

    Test commit

--- update-version.sh
+++ update-version.sh
@@ -4,7 +4,8 @@ echo $(date '+%Y-%m-%d %H:%M:%S') > version
 
 cd /cogs106/hw1
 git add --all
-git commit -m "Test commit" 
-git push https://github.com/annied471/cogs106.git origin main
+git commit -m "Test commit"
+git pull 
+git push https://github.com/annied471/cogs106.git main
 
 
